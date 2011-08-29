ac22c4923f0132a35208b1ab686aeadb62d5de85
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-08-29 12:12:31 +0100

    ActiveResourceKit, an Xcode framework project with unit tests

---
+++ ActiveResourceKitTests/ActiveResourceKitTests.h
@@ -0,0 +1,13 @@
+//
+//  ActiveResourceKitTests.h
+//  ActiveResourceKitTests
+//
+//  Created by Roy Ratcliffe on 29/08/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ActiveResourceKitTests : SenTestCase
+
+@end
---
+++ ActiveResourceKitTests/ActiveResourceKitTests.m
@@ -0,0 +1,32 @@
+//
+//  ActiveResourceKitTests.m
+//  ActiveResourceKitTests
+//
+//  Created by Roy Ratcliffe on 29/08/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import "ActiveResourceKitTests.h"
+
+@implementation ActiveResourceKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ActiveResourceKitTests");
+}
+
+@end
