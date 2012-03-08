68efac359238fb6959cef79a35c069b67a6afd0b
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-03-08 10:37:27 +0000

    Assert connection data and error assumptions

--- ActiveResourceKit/ARQueuedLoadingURLConnection.m
+++ ActiveResourceKit/ARQueuedLoadingURLConnection.m
@@ -30,6 +30,18 @@
 - (void)sendRequest:(NSURLRequest *)request completionHandler:(ARConnectionCompletionHandler)completionHandler
 {
 	[NSURLConnection sendAsynchronousRequest:request queue:[self operationQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+		// Wrap the response and body together using ARHTTPResponse. Operates
+		// more like Rails, where the response includes the body; but this
+		// involves a subtle semantic shift for Cocoa. Under Cocoa, nil data
+		// indicates an error. This is a subtle change in success-failure
+		// semantics. Cocoa's URL connection interfaces answer non-nil data on
+		// success, nil data on failure. Hence you would typically check for
+		// existence of data to determine success or failure. You can still do
+		// so by asking for the response body. However, you might also check for
+		// non-existing error assuming that nil data and non-nil error
+		// correspond to the same state, namely failure; while non-nil data and
+		// nil error correspond to success.
+		NSParameterAssert((data != nil && error == nil) || (data == nil && error != nil));
 		completionHandler([[ARHTTPResponse alloc] initWithURLResponse:response body:data], error);
 	}];
 }
