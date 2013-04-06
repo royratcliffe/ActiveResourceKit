887dfd6fe142fc99dfe8376fcb05f88fb16f1363
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-04-06 18:21:53 +0100

    Updates for Rails 3.2.13
    Using rake rails:update after updating Gemfile and re-bundling.

--- active-resource-kit-tests/config/initializers/session_store.rb
+++ active-resource-kit-tests/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ActiveResourceKitTests::Application.config.session_store :cookie_store, key: '_active-resource-kit-tests_session'
+ActiveResourceKitTests::Application.config.session_store :cookie_store, key: '_active_resource_kit_tests_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
