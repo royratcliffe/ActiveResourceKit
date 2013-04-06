3ecba0ea89f4aa212b05236ab341b4fb45d5a6f1
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-04-06 18:22:27 +0100

    Schema updated by Rails 3.2.13

--- active-resource-kit-tests/db/schema.rb
+++ active-resource-kit-tests/db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(:version => 20120409075740) do
   end
 
   create_table "people", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "name"
   end
 
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(:version => 20120409075740) do
     t.string   "title"
     t.text     "body"
     t.boolean  "published"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "poster_id"
   end
 
