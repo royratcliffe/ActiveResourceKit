8d92b5368c4a18a87e6d40dbd28f37c0f203dc97
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-05-21 14:48:19 +0100

    Comply with key-value observing protocol

--- ActiveResourceKit/ARResource.m
+++ ActiveResourceKit/ARResource.m
@@ -234,22 +234,37 @@ NSString *ARUndefinedKeyForGetterSelector(SEL selector);
 	return [_attributes objectForKey:[[ASInflector defaultInflector] underscore:key]];
 }
 
+// Removes a value if you set the value to nil. Complies with key-value
+// observing protocols by sending "will- and did-change value for key" messages.
+// Only send will-and-did-change messages if and only if the value changes. Use
+// -isEqual: to determine change versus no change.
 - (void)setValue:(id)value forUndefinedKey:(NSString *)key
 {
-	key = [[ASInflector defaultInflector] underscore:key];
 	if (value)
 	{
-		[_attributes setObject:value forKey:key];
+		NSString *attributeName = [[ASInflector defaultInflector] underscore:key];
+		if (![[_attributes objectForKey:attributeName] isEqual:value])
+		{
+			[self willChangeValueForKey:key];
+			[_attributes setObject:value forKey:attributeName];
+			[self didChangeValueForKey:key];
+		}
 	}
 	else
 	{
-		[_attributes removeObjectForKey:key];
+		[self setNilValueForKey:key];
 	}
 }
 
 - (void)setNilValueForKey:(NSString *)key
 {
-	[self setValue:[NSNull null] forKey:key];
+	NSString *attributeName = [[ASInflector defaultInflector] underscore:key];
+	if ([_attributes objectForKey:attributeName])
+	{
+		[self willChangeValueForKey:key];
+		[_attributes removeObjectForKey:attributeName];
+		[self didChangeValueForKey:key];
+	}
 }
 
 /**
